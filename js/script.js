9a7a6c97f2bceb1010619108014e75875f107a74
Katerina <katerina.raylyan@gmail.com>
2020-04-13 16:58:48 +0300

    feat: implement word card behavour

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//import cards from 'cards';
 
 function menuAnimate() {
     const navToggle = document.querySelector('.navigation__toggle');
@@ -16,3 +17,30 @@ function bgChange() {
   }
 }
 
+function flipCard() {
+  const card = document.querySelector('.card');
+  const leftButton = document.querySelector('.sound__button');
+  const rightButton = document.querySelector('.info__button');
+  card.classList.toggle('flipped');
+  rightButton.classList.toggle('hidden');
+  leftButton.classList.toggle('hidden');
+}
+
+function returnCard() {
+
+  
+  const card = document.querySelector('.card');
+  if(card.classList.contains('flipped')){
+    setTimeout(flipCard(),1000)
+  }
+}
+
+function playSound() {
+  const leftButton = document.querySelector('.sound__button');
+  const word = leftButton.dataset.word;
+  const url = `https://wooordhunt.ru/data/sound/word/uk/mp3/${word}.mp3`;
+
+  new Audio(url).play();
+  console.log(url);
+
+}
